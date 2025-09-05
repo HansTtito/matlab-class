44d25fa270110aa2a33ed10e1569416a79dea7b6
HansTtito <kvttitos@gmail.com>
2025-09-05 11:24:25 -0400

    tercera clase

--- clase_3/clase_3.m
+++ clase_3/clase_3.m
@@ -1,10 +1,14 @@
 % Clase 3
 
-a = 1;
-
 clear; % Limpia memoria
 
 close all; % cierra ventanaas
 
+% ST = Tendencia + ciclos + error
 
+% Define the components of the model
+
+trend = 1:10; % trend component
+cycles = sin(linspace(0, 2*pi, 10)); % Example cyclic component
+error = randn(1, 10); % Example error component
 
